7b760746bae5423b066d5ac7cc5214901e1d364d
Joerg Bellmann <joerg.bellmann@zalando.de>
2015-12-14 14:52:11 +0100

    proper names and fixing startup

--- stupsback-admin/src/main/java/org/zalando/stups/stupsback/admin/StupsBackAdminApplication.java
+++ stupsback-admin/src/main/java/org/zalando/stups/stupsback/admin/StupsBackAdminApplication.java
@@ -18,13 +18,9 @@ package org.zalando.stups.stupsback.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
-/**
- * Hello world!
- */
-
 @EnableAutoConfiguration
-public class App {
+public class StupsBackAdminApplication {
     public static void main(final String[] args) throws Exception {
-        SpringApplication.run(AppController.class, args);
+        SpringApplication.run(StupsBackAdminApplication.class, args);
     }
 }
