45a30c5ba50f24c5a8cba95d9ae2a9102f685714
Joerg Bellmann <joerg.bellmann@zalando.de>
2015-12-14 13:53:42 +0100

    initial setup ui, most taken from the tutorial

---
+++ stupsback-admin-ui/src/main/resources/static/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const React = require('react');
+const client = require('./client');
+
+const follow = require('./follow');
+
+const root = '/api';
+
+class App extends React.Component {
+}
+
+React.render(
+	<App />,
+	document.getElementById('react')
+)
