9912902ea758e312ab8ea39706370a3d2b2386ae
Christian Lohmann <christian.lohmann@zalando.de>
2015-12-18 11:17:16 +0100

    upgraded spring-webmvc

--- stupsback-admin/src/main/java/org/zalando/stups/stupsback/admin/CorsConfiguration.java
+++ stupsback-admin/src/main/java/org/zalando/stups/stupsback/admin/CorsConfiguration.java
@@ -15,15 +15,12 @@
  */
 package org.zalando.stups.stupsback.admin;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
- * 
+ *
  * @author jbellmann
  *
  */
