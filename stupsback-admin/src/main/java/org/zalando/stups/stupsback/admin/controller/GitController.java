0ea7128ea0b7b0374b2e4e2f2ef9d39f5a7e6642
Christian Lohmann <christian.lohmann@zalando.de>
2015-12-22 12:14:36 +0100

    #31 renamed controller

--- stupsback-admin/src/main/java/org/zalando/stups/stupsback/admin/controller/GitController.java
+++ stupsback-admin/src/main/java/org/zalando/stups/stupsback/admin/controller/GitController.java
@@ -1,11 +1,8 @@
 package org.zalando.stups.stupsback.admin.controller;
 
-import org.kohsuke.github.GHIssueBuilder;
 import org.kohsuke.github.GitHub;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.LinkDiscoverer;
 import org.springframework.web.bind.annotation.RestController;
-import org.zalando.stups.stupsback.admin.domain.Rating;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,7 +11,7 @@ import lombok.RequiredArgsConstructor;
  */
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class GithubController {
+public class GitController {
 
     private final GitHub gitHub;
 
